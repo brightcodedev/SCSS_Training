5a341712c6302dd095bfd86299a70f4dc0ca0878
werner33 <manley.preston@gmail.com>
2020-04-02 14:03:37 -0400

    set up user cards with basic BEM css

--- src/App.js
+++ src/App.js
@@ -2,23 +2,74 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let pilotData = {
+  name: "Jack Dawes",
+  role: "Pilot",
+  title: "Captain",
+  stats: {
+    milesFlown: "45,453",
+    flights: "345",
+    yearsWithCompany: "7"
+  },
+  image: "hanks.jpg"
+}
+
+let flightAttendantData = {
+  name: "Stefanie Ramirez",
+  role: "Flight Attendant",
+  title: "senior",
+  stats: {
+    milesFlown: "345,453",
+    flights: "2345",
+    yearsWithCompany: "18"
+  },
+  image: "flight_attendant.jpg"
+}
+
+let groundCrewData = {
+  name: "Joey Ripapelli",
+  role: "Ground Crew",
+  title: "rookie",
+  stats: {
+    milesFlown: "0",
+    flights: "345",
+    yearsWithCompany: "2"
+  },
+  image: "ground_crew.jpg"
+}
+
+const EmployeeCard = (props) => {
+  let employeeData = props.employeeData;
+  let stats = employeeData.stats;
+
+  return (
+    <div className="employeeCard">
+        <img className="employeeCard__profileImage" src={employeeData.image} />
+        <div className="employeeCard__name">{employeeData.name}</div>
+        <div className="employeeCard__title">{employeeData.title}</div>
+        <div className="employeeCard__stats">
+          <div>
+            <div>
+              {stats.milesFlown}
+            </div>
+            <div>
+              miles flown
+            </div>
+          </div>
+          <div>{stats.flights}</div>
+          <div>{stats.yearsWithCompany}</div>
+
+        </div>
+      </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EmployeeCard employeeData={pilotData} />
+      <EmployeeCard employeeData={flightAttendantData} />
+      <EmployeeCard employeeData={groundCrewData} />
     </div>
   );
 }
